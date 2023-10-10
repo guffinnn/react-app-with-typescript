b75426e461e8f21f3df885411b27a8d12ee4e070
Danik <dashkevich_daniil2006@mail.ru>
2023-10-10 17:42:52 +0300

    added ProductPage and some CSS styles

---
+++ src/components/Card.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+// Импортирую стили
+import './styles/Card.css';
+
+// Импортирую изображения для карточек
+import chair from './img/chair.png';
+import lamp from './img/lamp.png';
+import sofa from './img/sofa.png';
+import table from './img/table.png';
+
+// Объект для обращения к изображениям
+const images: {[imgKey: string]: string} = {
+    chair: chair,
+    lamp: lamp,
+    sofa: sofa,
+    table: table
+};
+
+interface CardProps {
+    imgKey: string;
+    name: string;
+    price: string;
+}
+
+// Функция добавления компонента Card
+const Card: React.FC<CardProps> = ({imgKey, name, price}) => {
+    const image = images[imgKey] || chair;
+    return (
+        <div className="card__frame">
+            <div className="image__frame">
+                <img src={image} className="image__card" alt={imgKey} />
+                <div className="subtext__frame">
+                    <p className="subtext__content">New</p>
+                </div>
+            </div>
+            <div className="text__content__frame">
+                <div className="heading__frame">
+                    <p className="heading__text">{price}</p>
+                </div>
+                <div className="description__frame">
+                    <p className="description__text">{name}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Импорт стилей CSS
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Импорт компонентов Card и ProductPage
+import Card from './components/Card';
+import ProductPage from "./components/ProductPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      {/*<Card imgKey='chair'
+            name='Стул, IKEA'
+            price='4999 Kc'/>
+      <Card imgKey='lamp'
+            name='Лампа, IKEA'
+            price='2999 Kc' />
+      <Card imgKey='sofa'
+            name='Диван, IKEA'
+            price='9999 Kc' />
+      <Card imgKey='table'
+            name='Стол, IKEA'
+            price='5999 Kc' />*/}
+      <ProductPage imgKey='lamp'
+                   name='Лампа, IKEA'
+                   price='2999 Kc' />
   </React.StrictMode>
 );
 
