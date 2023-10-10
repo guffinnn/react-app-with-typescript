846880f2171811e7771ce78e235bf80c84a273c7
Danik <dashkevich_daniil2006@mail.ru>
2023-10-10 13:19:21 +0300

    created a card component, added styles CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      <Card imgKey='chair'
+            name='Стул, IKEA'
+            price='4999 Kc'/>
+      <Card imgKey='lamp'
+            name='Лампа, IKEA'
+            price='2999 Kc' />
+      <Card imgKey='sofa'
+            name='Диван, IKEA'
+            price='9999 Kc' />
+      <Card imgKey='table'
+            name='Стол, IKEA'
+            price='5999 Kc' />
   </React.StrictMode>
 );
 
