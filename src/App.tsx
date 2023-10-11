b5a028c97492af6c12b3767cad31f1f2a3f91b5a
Danik <dashkevich_daniil2006@mail.ru>
2023-10-11 10:40:27 +0300

    save some files changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Card imgKey='chair'
+                  name='Стул, IKEA'
+                  price='4999 Kc'/>
+            <Card imgKey='lamp'
+                  name='Лампа, IKEA'
+                  price='2999 Kc' />
+            <Card imgKey='sofa'
+                  name='Диван, IKEA'
+                  price='9999 Kc' />
+            <Card imgKey='table'
+                  name='Стол, IKEA'
+                  price='5999 Kc' />
+        </div>
+    );
 }
 
 export default App;
